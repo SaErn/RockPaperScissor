7c80d06a49d574b63b7077d8ae9785250c23f769
SaErn <69966798+SaErn@users.noreply.github.com>
2020-08-28 10:05:43 +0200

    Add files via upload

--- gameStart.java
+++ gameStart.java
@@ -1,140 +1,242 @@
+import java.util.Random;
 import java.util.Scanner;
 
-public class gameStart{
-//	private String rock = "Rock";
-//	private String paper = "Paper";
-//	private String scissor = "Scissor";
+public class gameStart {
+	//Globala variabler för namn på valmöjligheterna i spelet, spelarnas poäng, och vilken för vilken runda i spelet 
+	private static String rock = "Rock";
+	private static String paper = "Paper";
+	private static String scissor = "Scissor";
+	private static int scoreOne = 0;
+	private static int scoreTwo = 0;
+	private static int roundCount = 1;
 
 	public static void main(String[] args) {
+		
+		//Skapar enbart en scanner här i main, skickas som argument till metoder som använder scanner
+		/*Jag valde att göra så här med scanner för när jag använder flera scanners och kör scanner.close() på dem så stänger det
+		 * hela "System.in" streamen så andra Scanners slutar fungera*/
 		Scanner input = new Scanner(System.in);
-		String playerOne;
-		String playerTwo;
-//		gameStart defaultRules = new gameStart();
-		nameChange rules = new nameChange();
-		int choice;
-		
-		
-		System.out.println("Welcome to Rock, Paper, Scissor Battle Royale\nInput 1 to start game\nInput 2 to change names");
-		choice = input.nextInt();
-		
-		if(choice == 1) {
-//			System.out.println("Game Start\n 1 = Rock\n 2 = Paper\n 3 = Scissor\n Enter name or number of your choice ");
-//			playerOne = input.next();
-//			playerTwo = input.next();
-//			answerCheck(playerOne, playerTwo, defaultRules.rock, defaultRules.paper, defaultRules.scissor);
-			gamePlayer(rules);
-		
-		}else if(choice == 2) {
-			rules = ruleSettings();
-			gamePlayer(rules);
-//			System.out.println("Game Start\n 1 = "+ newRules.rock + "\n 2 = "+ newRules.paper + "\n 3 = "+ newRules.scissor +"\n Enter name or number of your choice ");
-//			playerOne = input.next();
-//			playerTwo = input.next();
-//			answerCheck(playerOne, playerTwo, newRules.rock, newRules.paper, newRules.scissor);
+		//Objekt ur random-klassen som används för att ta fram slumpad int
+		Random generator = new Random();
+		//För val i meny
+		int choiceMenu = 0; 
+		//För loopar som håller spelets olika delar igång tills korrekt genomförda
+		boolean waiting = true; 
+		boolean programRunning = true; 
+		boolean gameOn = true; 
+
+		//Inväntar att programmet ska avslutas/köras färdigt
+		while (programRunning) {
+			waiting = true;
+			System.out.println("Welcome to " + gameStart.rock + ", " + gameStart.paper + ", " + gameStart.scissor
+					+ " Battle Royale\n" + "Input 1 to start game with 2 Human player\n"
+					+ "Input 2 to play against AI\n" + "Input 3 to change names");
+			
+			choiceMenu = playerInput(input);
+
+//			//Loop som inväntar svar i första menyn
+//			while (waiting) {
+//				
+//				//Fångar upp felaktigt inmatad input. Om formateringsfel upptäcks ombes användaren göra nytt försök
+//				try { 
+//					//Försöker omvandla input till int
+//					choiceMenu = Integer.parseInt(input.nextLine()); 
+//					
+//					//Om input korrekt och matchar ett av valbara alternativ bryts loopen
+//					if (choiceMenu >= 1 && choiceMenu <= 3) {
+//						waiting = false; 
+//					} else {
+//						System.out.println("Your input must be a number between 1-3");
+//					}
+//				
+//				} catch (NumberFormatException e) {
+//					System.out.println("Your input must be a number between 1-3");
+//				}
+//			}
+
+			//Val 1 i menyn -- Två mänskliga spelare
+			if (choiceMenu == 1) { 
+				
+				//Inväntar att spelrunda ska avslutas. En loop en omgång i spelet
+				while (gameOn) {
+					System.out.println("\nRound " + gameStart.roundCount);
+					//Anropar metod som kör spelrunda och skickar scanner som argument
+					gamePlayer(input); 
+					//Global variabel för vilken spelrunda inkrementeras med 1 för varje loop
+					gameStart.roundCount += 1; 
+					
+					//Om 3 omgångar har spelats testar programmet om rundan ska avslutas
+					if (gameStart.roundCount >= 4) { 
+						//Om spelarnas poäng ej är samma avslutas omgången och programmet
+						if (gameStart.scoreOne != gameStart.scoreTwo) { 
+							programRunning = false;
+							gameOn = false;
+							//Metod som skriver ut spelarnas slutgiltiga poäng
+							scorePrinter();
+						//Om spelarnas poäng lika körs en ny omgång och meddelande skrivs ut
+						} else if (gameStart.scoreOne == gameStart.scoreTwo) {
+							System.out.println("Tie Score! Sudden death round");
+						}
+					}
+				}
+				
+			//Val 2 i menyn -- Spela mot AI
+			} else if (choiceMenu == 2) { 
+				
+				while (gameOn) {
+					System.out.println("\nRound " + gameStart.roundCount);
+					
+					//Metod för spelrunda mot AI. Tar Scanner och Random som argument
+					gamePlayerAi(input, generator); 
+					
+					//Stycket nedanför är samma kod som ovan
+					gameStart.roundCount += 1;
+					if (gameStart.roundCount >= 4) {
+						if (gameStart.scoreOne != gameStart.scoreTwo) {
+							programRunning = false;
+							gameOn = false;
+							scorePrinter();
+						} else if (gameStart.scoreOne == gameStart.scoreTwo) {
+							System.out.println("Tie Score! Sudden death round");
+						}
+					}
+				}
+			
+			//Val 3 i menyn - Byt namn på spelalternativen
+			} else if (choiceMenu == 3) { 
+				//Metod för byta namn. Tar Scanner som argument
+				ruleSettings(input);
+			}
 		}
+	}
+	
+	//Skriver ut spelarnas poäng efter avsluta runda
+	public static void scorePrinter() {
+		System.out.println(
+				"\nGame over!\nPlayer 1 score: " + gameStart.scoreOne + "\nPlayer 2 score: " + gameStart.scoreTwo);
 		
+		//Om spelare ett har mer poäng än spelare två
+		if (gameStart.scoreOne > gameStart.scoreTwo) { 
+			System.out.println("Player 1 is the Winner!");
+		} else {
+			System.out.println("Player 2 is the Winner!");
+		}
+
+	}
+	
+	//Kör spelrunda med två mänskliga spelare
+	public static void gamePlayer(Scanner input) { 
+		//Håller spelarnas inmatade val
+		int playerOne;
+		int playerTwo;
+
+		//Skriver ut alternativen i spelet med default- eller ändrade namn
+		System.out.println("Game Start\n 1 = " + gameStart.rock + "\n 2 = " + gameStart.paper + "\n 3 = "
+				+ gameStart.scissor + "\nEnter number of your choice ");
+		
+		//Kör metoder som tar emot int-värde från spelarna och lagrar i respektive variabel
+		playerOne = playerInput(input);
+		playerTwo = playerInput(input);
+		
+		//Metod som testar vem som vann omgången. Skickar spelarnas valda alternativ som argument
+		answerCheck(playerOne, playerTwo);
 	}
 
-	public static void gamePlayer(nameChange chosenRules) {
-		String playerOne;
-		String playerTwo;
-		Scanner inputGame = new Scanner(System.in);
+	//Kör spelrunda med människa mot AI spelare
+	public static void gamePlayerAi(Scanner input, Random generator) {
+		int playerOne;
+
+		System.out.println("Game Start\n 1 = " + gameStart.rock + "\n 2 = " + gameStart.paper + "\n 3 = "
+				+ gameStart.scissor + "\nEnter number of your choice ");
 		
-		//System.out.println("Game Start\n 1 = "+ chosenRules.rock + "\n 2 = "+ chosenRules.paper + "\n 3 = "+ chosenRules.scissor +"\n Enter name or number of your choice ");
-		playerOne = inputGame.nextLine();
-		playerTwo = inputGame.nextLine();
-		answerCheck(playerOne, playerTwo, chosenRules.rock, chosenRules.paper, chosenRules.scissor);
-		inputGame.close();
+		playerOne = playerInput(input);
+		
+		//Testar vem som vann omgång men här är ett av argumenten det returnerade värdet från metod som tar fram slumpad int 1-3
+		answerCheck(playerOne, randomNumber(generator));
 	}
-	
-//	public static void gamePlayer(nameChange chosenRules) {
-//		String playerOne;
-//		String playerTwo;
-//		Scanner inputGame = new Scanner(System.in);
-//		
-//		System.out.println("Game Start\n 1 = "+ chosenRules.rock + "\n 2 = "+ chosenRules.paper + "\n 3 = "+ chosenRules.scissor +"\n Enter name or number of your choice ");
-//		playerOne = inputGame.next();
-//		playerTwo = inputGame.next();
-//		answerCheck(playerOne, playerTwo, chosenRules.rock, chosenRules.paper, chosenRules.scissor);
-//		inputGame.close();
-//	}
-	
-	public static void randomNumber() {
-		
+
+	//Sköter input från spelarna
+	public static int playerInput(Scanner input) {
+
+		int playerInput = 0; 
+		boolean chosing = true;
+
+		//Körs tills val inmatat korrekt
+		while (chosing) { 
+			try {
+				playerInput = Integer.parseInt(input.nextLine());
+				if (playerInput >= 1 && playerInput <= 3) {
+					chosing = false;
+				} else {
+					System.out.println("Your input must be a number between 1-3");
+				}
+
+			} catch (NumberFormatException e) {
+				System.out.println("Your input must be a number between 1-3");
+			}
+		}
+		return playerInput;
 	}
-	
-	public static nameChange ruleSettings() {
-		String rockNewName;
-		String paperNewName;
-		String scissorNewName;
-		Scanner inputRules = new Scanner(System.in);
-		nameChange newRules = new nameChange();
-		
+
+	public static int randomNumber(Random generator) {
+		int aiChoice;
+		aiChoice = generator.nextInt(3) + 1;
+		return aiChoice;
+	}
+
+	public static void ruleSettings(Scanner input) {
+
 		System.out.println("Enter new name for ROCK: ");
-		rockNewName = inputRules.next();
+		gameStart.rock = input.nextLine();
+
 		System.out.println("Enter new name for PAPER: ");
-		paperNewName = inputRules.next();
+		gameStart.paper = input.nextLine();
+
 		System.out.println("Enter new name for SCISSOR: ");
-		scissorNewName = inputRules.next();
-		
-		newRules = new nameChange(rockNewName, paperNewName, scissorNewName);
-		inputRules.close();
-		return newRules;
+		gameStart.scissor = input.nextLine();
 	}
-	
-	public static void answerCheck(String playerOneAns, String playerTwoAns, String rockName, String paperName, String scissorName) {
-		if(playerOneAns.equals("1") || playerOneAns.equalsIgnoreCase(rockName)) {
-			if(playerTwoAns.equals("1") || playerTwoAns.equalsIgnoreCase(rockName)) {
-				System.out.println("Draw");
-			}
-			else if(playerTwoAns.equals("2") || playerTwoAns.equalsIgnoreCase(paperName)) {
-				System.out.println("Player Two Wins!");
-			}
-			else if(playerTwoAns.equals("3") || playerTwoAns.equalsIgnoreCase(scissorName)) {
-				System.out.println("Player One Wins!");
-			}
-		}
-		else if(playerOneAns.equals("2") || playerOneAns.equalsIgnoreCase(paperName)) {
-			if(playerTwoAns.equals("1") || playerTwoAns.equalsIgnoreCase(rockName)) {
-				System.out.println("Player One Wins!");
-			}
-			else if(playerTwoAns.equals("2") || playerTwoAns.equalsIgnoreCase(paperName)) {
-				System.out.println("Draw");
-			}
-			else if(playerTwoAns.equals("3") || playerTwoAns.equalsIgnoreCase(scissorName)) {
-				System.out.println("Player Two Wins!");
-			}
-		}
-		else if(playerOneAns.equals("3") || playerOneAns.equalsIgnoreCase(scissorName)) {
-			if(playerTwoAns.equals("1") || playerTwoAns.equalsIgnoreCase(rockName)) {
-				System.out.println("Player Two Wins!");
-			}
-			else if(playerTwoAns.equals("2") || playerTwoAns.equalsIgnoreCase(paperName)) {
-				System.out.println("Player One Wins!");
-			}
-			else if(playerTwoAns.equals("3") || playerTwoAns.equalsIgnoreCase(scissorName)) {
-				System.out.println("Draw");
-			}
+
+	public static void answerCheck(int playerOneAns, int playerTwoAns) {
+
+		System.out.println(playerOneAns + " " + playerTwoAns);
+
+		if (playerOneAns == 1 && playerTwoAns == 1) { // Rock vs Rock
+			System.out.println(
+					"Player 1 chose: " + gameStart.rock + "\nPlayer 2 chose: " + gameStart.rock + "\nIt's a draw!");
+
+		} else if (playerOneAns == 1 && playerTwoAns == 2) { // Rock vs Paper
+			System.out.println(
+					"Player 1 chose: " + gameStart.rock + "\nPlayer 2 chose: " + gameStart.paper + "\nPlayer 2 wins!");
+			gameStart.scoreTwo += 1;
+
+		} else if (playerOneAns == 1 && playerTwoAns == 3) { // Rock vs Scissor
+			System.out.println("Player 1 chose: " + gameStart.rock + "\nPlayer 2 chose: " + gameStart.scissor
+					+ "\nPlayer 1 wins!");
+			gameStart.scoreOne += 1;
+		} else if (playerOneAns == 2 && playerTwoAns == 1) { // Paper vs Rock
+			System.out.println(
+					"Player 1 chose: " + gameStart.paper + "\nPlayer 2 chose: " + gameStart.rock + "\nPlayer 1 wins!");
+			gameStart.scoreOne += 1;
+		} else if (playerOneAns == 2 && playerTwoAns == 2) { // Paper vs Paper
+			System.out.println(
+					"Player 1 chose: " + gameStart.paper + "\nPlayer 2 chose: " + gameStart.paper + "\nIt's a draw!");
+
+		} else if (playerOneAns == 2 && playerTwoAns == 3) { // Paper vs Scissor
+			System.out.println("Player 1 chose: " + gameStart.paper + "\nPlayer 2 chose: " + gameStart.scissor
+					+ "\nPlayer 2 wins!");
+			gameStart.scoreTwo += 1;
+		} else if (playerOneAns == 3 && playerTwoAns == 1) { // Scissor vs Rock
+			System.out.println("Player 1 chose: " + gameStart.scissor + "\nPlayer 2 chose: " + gameStart.rock
+					+ "\nPlayer 2 wins!");
+			gameStart.scoreTwo += 1;
+		} else if (playerOneAns == 3 && playerTwoAns == 2) { // Scissor vs Paper
+			System.out.println("Player 1 chose: " + gameStart.scissor + "\nPlayer 2 chose: " + gameStart.paper
+					+ "\nPlayer 1 wins!");
+			gameStart.scoreOne += 1;
+		} else if (playerOneAns == 3 && playerTwoAns == 3) { // Scissor vs Scissor
+			System.out.println("Player 1 chose: " + gameStart.scissor + "\nPlayer 2 chose: " + gameStart.scissor
+					+ "\nIt's a Draw!");
 		}
 
 	}
-}
-
-class nameChange{
-	String rock = "Rock";
-	String paper = "Paper";
-	String scissor = "Scissor";
-	
-	public nameChange() {
-		rock = "Rock";
-		paper = "Paper";
-		scissor = "Scissor";
-	}
-	
-	public nameChange(String newRock, String newPaper, String newScissor) {
-	    rock = newRock;
-	    paper = newPaper;
-	    scissor = newScissor; 
-	  }
-
 }
